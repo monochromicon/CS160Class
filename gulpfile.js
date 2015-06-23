ac53ffe3ab59f225d7a4eca2d55fcbf68eb4b77a
Graham Barber <grahammbarber@gmail.com>
2015-06-23 09:41:05 -0700

    Broke up build command, added file watching

--- gulpfile.js
+++ gulpfile.js
@@ -6,29 +6,44 @@ var gulp       = require("gulp"),
 	connect    = require("gulp-connect"),
 	rsync      = require("gulp-rsync");
 	
-gulp.task("default", function () {
+gulp.task("buildCSS", function () {
 	gulp.src("src/css/index.css")
 		.pipe(minifyCSS())
-		.pipe(gulp.dest("dist/public/css/index.css"));
-		
+		.pipe(gulp.dest("dist/public/css/"))
+		.pipe(connect.reload());
+});
+
+gulp.task("buildAng", function () {
 	gulp.src("src/js/app.js")
 		.pipe(uglify())
-		.pipe(gulp.dest("dist/public/js/app.js"));
-		
+		.pipe(gulp.dest("dist/public/js/"))
+		.pipe(connect.reload());
+});
+
+gulp.task("buildServ", function () {
 	gulp.src("src/server.js")
 		.pipe(uglify())
-		.pipe(gulp.dest("dist/server.js"));
-		
+		.pipe(gulp.dest("dist/"))
+		.pipe(connect.reload());
+});
+
+gulp.task("buildHTML", function () {
 	gulp.src("src/index.jade")
 		.pipe(jade())
 		.pipe(minifyHTML())
-		.pipe(gulp.dest("dist/public/index.html"));
-		
+		.pipe(gulp.dest("dist/public/"))
+		.pipe(connect.reload());
+});
+
+gulp.task("buildPages", function () {
 	gulp.src("src/pages/**.jade")
-		.pipe(jade)
+		.pipe(jade())
 		.pipe(minifyHTML())
-		.pipe(gulp.dest("dist/public/pages/"));
-	});
+		.pipe(gulp.dest("dist/public/pages/"))
+		.pipe(connect.reload());
+});
+
+gulp.task("build", ["buildCSS", "buildAng", "buildServ", "buildHTML", "buildPages"]);
 
 gulp.task("deploy", function () {
 	gulp.src("dist/")
@@ -43,7 +58,16 @@ gulp.task("deploy", function () {
 
 gulp.task("connect", function() {
 	connect.server({
-		root: "app",
-		livereload: true
+		root: "dist/public",
+		livereload: true,
+		port: 8080
 	});
+});
+
+gulp.task("default", ["build", "connect"], function () {
+	gulp.watch("src/index.jade", ["buildHTML"]);
+	gulp.watch("src/pages/**.jade", ["buildPages"]);
+	gulp.watch("src/js/app.js", ["buildAng"]);
+	gulp.watch("src/server.js", ["buildServ"]);
+	gulp.watch("src/css/index.css", ["buildCSS"]);
 });
