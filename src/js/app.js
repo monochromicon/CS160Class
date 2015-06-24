290503b6a1811bd684e2d633ab65ce4078886736
Graham Barber <grahammbarber@gmail.com>
2015-06-24 15:49:37 -0700

    Configured relative linking

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,35 @@ $(document).ready(function () {
 
 var page = angular.module("webpage", ["ngRoute"]);
 
+page.config(function ($routeProvider, $locationProvider) {
+	$routeProvider
+		.when("/", {
+			templateUrl: "pages/home.html",
+			controller: "mainController"
+	})
+		.when("/announcements", {
+			templateUrl: "pages/home.html",
+			controller: "mainController"
+	})
+		.when("/assignments", {
+			templateUrl: "pages/assignments.html",
+			controller: "assignmentController"
+	})
+		.when("/forum", {
+			templateUrl: "pages/forum.html",
+			controller: "forumController"
+	});
+	
+});
+
 page.controller("mainController", ["$scope", function ($scope) {
-	$scope.text = "Here is some text!";
+	$scope.text1 = "Announcement! Announcement!";
+}]);
+
+page.controller("assignmentController", ["$scope", function ($scope) {
+	$scope.text2 = "Do some stuff! Do some other stuff!";
+}]);
+
+page.controller("forumController", ["$scope", function ($scope) {
+	$scope.text3 = "Talk amongst yourselves!";
 }]);
