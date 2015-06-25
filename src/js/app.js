e6bc93a99abfa0715ae7722f61d68a0556f6a7ab
Graham Barber <grahammbarber@gmail.com>
2015-06-25 16:20:26 -0700

    Solved the mystery of ugly URLS

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,19 @@
 $(document).ready(function () {
 	$(".button-collapse").sideNav();
+	
 });
 
 var page = angular.module("webpage", ["ngRoute"]);
 
-page.config(function ($routeProvider, $locationProvider) {
+page.config(function ($routeProvider) {
 	$routeProvider
 		.when("/", {
 			templateUrl: "pages/home.html",
-			controller: "mainController"
+			controller: "announcementController"
 	})
 		.when("/announcements", {
 			templateUrl: "pages/home.html",
-			controller: "mainController"
+			controller: "announcementController"
 	})
 		.when("/assignments", {
 			templateUrl: "pages/assignments.html",
@@ -21,11 +22,19 @@ page.config(function ($routeProvider, $locationProvider) {
 		.when("/forum", {
 			templateUrl: "pages/forum.html",
 			controller: "forumController"
-	});
+		});
+	
+	
+//	When HTML5 Mode is enabled, configure URL rewriting server-side
+//	$routeProvider.html5Mode(true);
 	
 });
 
 page.controller("mainController", ["$scope", function ($scope) {
+	$scope.text = "Here are some static words.";
+}]);
+
+page.controller("announcementController", ["$scope", function ($scope) {
 	$scope.text1 = "Announcement! Announcement!";
 }]);
 
